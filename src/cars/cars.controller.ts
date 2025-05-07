caa1ee5a30dd2e5e353e9c7d062f9ed3b2153109
Uriel Mora <upm1808@gmail.com>
2025-05-07 15:51:11 -0600

    Feat:Update car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -27,10 +28,11 @@ export class CarsController {
 
     @Patch(':id')
     updatePCar(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() body: any)
+        @Param('id', ParseUUIDPipe) id: string,
+        @Body() updateCarDto: UpdateCarDto )
         {
-        return body;
+            
+        return updateCarDto;
     }
     @Delete(':id')
     deleteCar(@Param('id', ParseIntPipe) id: number){
