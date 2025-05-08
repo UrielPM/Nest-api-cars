884c75ab1170e5c6343afbc19aa46390f496434a
Uriel Mora <upm1808@gmail.com>
2025-05-08 17:50:13 -0600

    Feat: Crud complete brand- resource

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,1 +1,11 @@
-export class CreateBrandDto {}
+import { IsString, MinLength } from "class-validator";
+
+export class CreateBrandDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+    
+
+
+}
