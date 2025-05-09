ea08120198b7cb49eb0f8612b396d4e099ee49ef
Uriel Mora <upm1808@gmail.com>
2025-05-09 12:17:32 -0600

    Feat: Inject service into another service

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from 'src/brands/brands.service';
+import { BRAND_SEED } from './data/brand.seed';
 
 
 @Injectable()
 export class SeedService {
   
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
 
+  ){}
   populateDB(){
 
+    this.carsService.fillCarsWithSeedData( CARS_SEED);
+    this.brandsService.fillCarsWithSeedData(BRAND_SEED);
 
     return 'Seed executed ';
   }
